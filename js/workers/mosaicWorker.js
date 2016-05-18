8d4a01c601b778f4d427e107ff404b2696e84b91
Gongzhi Kan <kangongzhi@gmail.com>
2016-05-18 21:39:07 +1000

    added more comments in mosaic worker

--- js/workers/mosaicWorker.js
+++ js/workers/mosaicWorker.js
@@ -18,15 +18,15 @@ onmessage = function (e) {
         tileWidth = data.tileWidth,
         canvasWidth = data.canvasWidth,
         canvasHeight = data.canvasHeight,
-        tileRowYInImg = data.tileRowYInImg, // tile's y in the image = row num of current row * tile height
-        currentTileXInImg = 0,
+        tileRowYInImg = data.tileRowYInImg,// tile's y in the image = row num of current row * tile height
+        currentTileXInImg = 0,// tile's x in the image = row num of current row * img width
         pixelData = imgData.data,
         numOfTilesInRow = data.numOfTilesX,
-        hexColor = '',
-        svgContent = '',
-        completedAjax = 0,
-        rowContent = [],
-        currentTileIndex = 0,
+        hexColor = '',// the hex color code that will be used in the color url, converted from the avg rgb
+        svgContent = '',// the svg images composing the tile row
+        completedAjax = 0,// the counter of the ajax calls, to track how many has completed
+        rowContent = [],// stores the svg images in the order of tile position in the row
+        currentTileIndex = 0,// current tile's index in the tile row, 0 - (number of tiles in the row -1)
         avgRGB = {},
         currentTile = new Tile(tileWidth, tileHeight),
         mosaicRowUI = new MosaicRowUI();//set the default tile width and tile height
@@ -36,22 +36,27 @@ onmessage = function (e) {
         currentTile.height = canvasHeight - tileRowYInImg;
     }
 
-    //calculate avg color
-    for (currentTileIndex; currentTileIndex < numOfTilesInRow; currentTileIndex += 1) { // loop over each tile in the row
-        currentTileXInImg = currentTileIndex * tileWidth; // reset current tile row X in img
+    // calculate avg color
+    for (currentTileIndex; currentTileIndex < numOfTilesInRow; currentTileIndex += 1) {// loop over each tile in the row
+        currentTileXInImg = currentTileIndex * tileWidth;// reset current tile row X in img
         // adjust tile width
         if (currentTileXInImg + tileWidth > canvasWidth) {
             currentTile.width = canvasWidth - currentTileXInImg;
         } else {
             currentTile.width = tileWidth;
         }
-        //calculate avg rgb of current tile
+        // calculate avg rgb of current tile
         avgRGB = currentTile.getAvgRBG(pixelData, canvasWidth, currentTileIndex, tileWidth);
+        // convert rgb to hex
         hexColor = ColorTool.rgbToHex(avgRGB.r, avgRGB.g, avgRGB.b);
+        // fetch tile img from server
+        /**
+         * @todo can use tile image pool(cached tile images) to improve performance
+         */
         MosaicWorkerHelper.getTile(hexColor, currentTileIndex).then(function (response) {
             rowContent[response.index] = response.response;
             completedAjax += 1;
-            if (completedAjax === numOfTilesInRow) { // all promises have been completed
+            if (completedAjax === numOfTilesInRow) {// all promises have been completed
                 svgContent = rowContent.join('');
                 MosaicWorkerHelper.returnImgRow(mosaicRowUI, svgContent);
             }
