7ad193c3cf14fe7450fe81092f86e9c6b518b9dc
Gongzhi Kan <kangongzhi@gmail.com>
2016-05-18 14:59:15 +1000

    added js doc for colorTool.js

--- js/modules/colorTool.js
+++ js/modules/colorTool.js
@@ -1,3 +1,7 @@
+/**
+ * ColorTool module that helps doing color conversions or other color related tasks upon needs
+ * @module ColorTool
+ */
 var ColorTool = (function () {
     'use strict';
 
@@ -5,13 +9,26 @@ var ColorTool = (function () {
         publicObj = {}; // public API interface obj
 
     privateObj = {
-        componentToHex: function (c) {
-            var hex = c.toString(16);
+        /**
+         * Converts a component number to a hex number
+         * @returns {string}
+         * @param {number} componentNumber
+         */
+        componentToHex: function (componentNumber) {
+            var hex = componentNumber.toString(16);
             return hex.length === 1 ? "0" + hex : hex;
         }
     };
 
     publicObj = {
+        /**
+         * Converts RGB values to hex color code
+         * needs {@link componentToHex}
+         * @param r
+         * @param g
+         * @param b
+         * @returns {*}
+         */
         rgbToHex: function (r, g, b) {
             return privateObj.componentToHex(r) + privateObj.componentToHex(g) + privateObj.componentToHex(b);
 
